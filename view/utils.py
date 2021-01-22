2ee252723753bc1f409952332c8443774a3a857f
Shawon Ashraf <shawon13@live.com>
2021-01-22 02:22:11 +0100

    validate xml

--- view/utils.py
+++ view/utils.py
@@ -1,3 +1,6 @@
+import xmlschema
+
+
 def get_hashtag_from_text(text):
     hashtags = set()
 
@@ -10,13 +13,14 @@ def get_hashtag_from_text(text):
     return hashtags
 
 
+# validates xml against a schema
+# params: path to xml, path to schema file(xsd)
+def validate_xml(xml_file_path, schema_file_path):
+    schema = xmlschema.XMLSchema(schema_file_path)
+    return schema.is_valid(xml_file_path)
 
 # text = "\\xf0\\x9f\\x98\\x82\\n\\n\\xf0\\x9f\\x8e\\xa5"
 # d = "b'Video Game Fan | 19 | \\xe2\\x99\\x82| Nintendo | RPGs | Fighting Games | Etc. |'"
 # print(d.encode("utf-8"))
 #
 # text2 = "Diving back into #Cyberpunk2077 tonight on my #Twitch. #gaming #atreaming #cyberpunk #ps4"
-
-
-
-
